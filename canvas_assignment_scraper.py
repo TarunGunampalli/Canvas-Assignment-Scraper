bc8adef352bac2777eab73dd84c8bdda59396dd3
TarunGunampalli <34664042+TarunGunampalli@users.noreply.github.com>
2020-09-15 14:22:29 -0500

    Update canvas_assignment_scraper.py

--- canvas_assignment_scraper.py
+++ canvas_assignment_scraper.py
@@ -4,7 +4,7 @@ from canvasapi import Canvas
 # Canvas API URL
 API_URL = "https://utexas.instructure.com"
 # Canvas API key
-API_KEY = "1017~1Wi1q7dm8WaLjbrdqQsP8lpBafuw4LYVSOgi8xxjmFQSSGl9G5UHR0J5VvDfS36z"
+API_KEY = <KEY>
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
