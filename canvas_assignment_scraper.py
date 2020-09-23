b6cfef04bb1ba0b4e999daecfcee024853c7d470
TarunGunampalli <34664042+TarunGunampalli@users.noreply.github.com>
2020-09-22 19:28:29 -0500

    Using assignment groups and getting due dates

--- canvas_assignment_scraper.py
+++ canvas_assignment_scraper.py
@@ -4,7 +4,7 @@ from canvasapi import Canvas
 # Canvas API URL
 API_URL = "https://utexas.instructure.com"
 # Canvas API key
-API_KEY = <KEY>
+API_KEY = "1017~1Wi1q7dm8WaLjbrdqQsP8lpBafuw4LYVSOgi8xxjmFQSSGl9G5UHR0J5VvDfS36z"
 
 # Initialize a new Canvas object
 canvas = Canvas(API_URL, API_KEY)
@@ -13,6 +13,6 @@ canvas = Canvas(API_URL, API_KEY)
 courses = canvas.get_courses()
 
 for course in courses:
-    assignments = course.get_assignments()
-    for assignment in assignments:
-        print(assignment)
+    assignment_groups = course.get_assignment_groups(include=["assignments","all_dates"])
+    for assignment_group in assignment_groups:
+        print(assignment_group)
