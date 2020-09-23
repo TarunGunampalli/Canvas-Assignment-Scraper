375e03041f5f03a53c3f4575bcaca1f2d210d499
TarunGunampalli <34664042+TarunGunampalli@users.noreply.github.com>
2020-09-22 23:47:16 -0500

    Switched canvasapi python module to requests module
    The requests module allows me to access the due dates of each assignment

--- canvas_assignment_scraper.py
+++ canvas_assignment_scraper.py
@@ -1,36 +1,26 @@
 # Import the Canvas module
-from canvasapi import Canvas
 import requests
 
 # Canvas API URL
 API_URL = "https://utexas.instructure.com"
 # Canvas API key
 API_KEY = "1017~1Wi1q7dm8WaLjbrdqQsP8lpBafuw4LYVSOgi8xxjmFQSSGl9G5UHR0J5VvDfS36z"
+# My User ID
+USER_ID = "4345098"
 
-# Initialize a new Canvas object
-canvas = Canvas(API_URL, API_KEY)
+headers = {"Authorization": "Bearer " + API_KEY}
 
-user = canvas.get_user(4345098)
-courses = canvas.get_courses()
+courses = requests.get(API_URL + "/api/v1/courses", headers=headers)
 
-events = user.get_calendar_events_for_user(submission_types=["assignment"])
+for course in courses.json():
+    assignments = requests.get(API_URL + "/api/v1/users/" + USER_ID + "/courses/" + str(course["id"]) + "/assignments", headers=headers)
+    for assignment in assignments.json():
+        #print(assignment)
+        if not assignment["due_at"] is None:
+            print(assignment["name"] + "\t\t" + assignment["due_at"])
+            print(assignment["html_url"])
 
-print(events)
 
-for event in events:
-    print(event)
-
-# headers = {"Authorization": "Bearer " + API_KEY}
-
-# for course in courses:
-#     print(course.get_user_in_a_course_level_assignment_data(user))
-
-# for course in courses:
-#     assignments = requests.get("https://utexas.instructure.com//api/v1/courses/1290172/assignments")
-#     print(assignments.text)
-
-# for course in courses:
-#     assignments = user.get_assignments(course)
-#     print(assignments)
-#     for assignment in assignments:
-#         print(assignment)
+request = requests.get(API_URL + "/api/v1/users/" + USER_ID + "/courses/:course_id/assignments", headers=headers)
+# for todo in request:
+#     print(todo)
